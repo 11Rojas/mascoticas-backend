10f5f86b883bcd726953b5e959939b7f0924e816
11Rojas <119141741+11Rojas@users.noreply.github.com>
2026-02-21 18:17:37 -0400

    First commit in backend

--- gen-keys.js
+++
@@ -1,5 +0,0 @@
-const webpush = require('web-push');
-const fs = require('fs');
-const keys = webpush.generateVAPIDKeys();
-fs.writeFileSync('keys.txt', `PUBLIC_KEY=${keys.publicKey}\nPRIVATE_KEY=${keys.privateKey}`);
-console.log('Keys written to keys.txt');
